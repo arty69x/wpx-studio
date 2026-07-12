871cc11f4736daeb16ff74fc38f3694ab575d49b
arty69x <arty69xx@gmail.com>
2026-07-12 09:36:34 +0700

    feat: add collector account workspace routes

---
+++ app/account/[[...section]]/page.tsx
@@ -0,0 +1,6 @@
+import { PlatformExperience } from '@/components/artyverse/PlatformExperience';
+import '../../platform.css';
+
+export default function AccountWorkspace() {
+  return <PlatformExperience role="account" />;
+}
