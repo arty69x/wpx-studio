71aefc3abaaa7f433298bbf055e07ebabf3eb28b
arty69x <arty69xx@gmail.com>
2026-07-12 09:26:08 +0700

    fix: load marketplace primitives in checkout

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,5 @@
 import { CheckoutExperience } from '@/components/artyverse/CommerceExperience';
+import '../marketplace/marketplace.css';
 import '../cart/commerce.css';
 
 export default function CheckoutPage() {
