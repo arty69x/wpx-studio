a54e33d5e5a3e9d73cc0918a299239a1b369d622
arty69x <arty69xx@gmail.com>
2026-07-12 09:36:48 +0700

    feat: add admin operations workspace routes

---
+++ app/admin/[[...section]]/page.tsx
@@ -0,0 +1,6 @@
+import { PlatformExperience } from '@/components/artyverse/PlatformExperience';
+import '../../platform.css';
+
+export default function AdminWorkspace() {
+  return <PlatformExperience role="admin" />;
+}
